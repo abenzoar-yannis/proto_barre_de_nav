815d0924d1d886de8892c80a6a8c4cb57e9bde33
abenzoar yannis <abenzoaryannis.dev@gmail.com>
2022-01-11 16:03:38 +0100

    ajustement des class et optimisation css

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const sideMenuIn = document.getElementById("sideMenuIn");
 const sideMenuOut = document.getElementById("sideMenuOut");
-const sideMenu = document.querySelector(".sideMenu");
+const sideMenu = document.querySelector(".navBar__sideMenu");
 const tabs = document.querySelector(".tabs");
 
 sideMenuIn.addEventListener("click", () => {
-  sideMenu.classList.add("menuTranslate");
+  sideMenu.classList.remove("menuTranslate");
 });
 sideMenuOut.addEventListener("click", () => {
-  sideMenu.classList.remove("menuTranslate");
+  sideMenu.classList.add("menuTranslate");
 });
